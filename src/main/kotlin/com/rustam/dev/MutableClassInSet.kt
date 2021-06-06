12c5982293f38206d30af391dab34bbac11c186d
rustam <kuramshin.py@yandex.ru>
2021-06-06 23:05:35 +0300

    commit

---
+++ src/main/kotlin/com/rustam/dev/MutableClassInSet.kt
@@ -0,0 +1,20 @@
+package com.rustam.dev
+
+data class Person(var name: String, var age: Int)
+
+fun main() {
+
+    val person1 = Person("John", 20)
+    val person2 = Person("Doe", 30)
+
+    val personSet = setOf(person1, person2)
+
+    person1.name = "Jack"
+
+    if (person1 in personSet) {
+        println("found")
+    } else {
+        println("NOT found")
+    }
+
+}
