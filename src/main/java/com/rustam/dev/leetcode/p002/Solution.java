daa221390acd4445e99aea89bfd61810e111c112
rustam <kuramshin.py@yandex.ru>
2022-01-11 01:16:46 +0300

    commit

---
+++ src/main/java/com/rustam/dev/leetcode/p002/Solution.java
@@ -0,0 +1,29 @@
+package com.rustam.dev.leetcode.p002;
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new ListNode();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
