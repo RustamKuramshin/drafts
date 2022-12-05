f035ce13742770435e538cf03e978e0d3dd1f4c5
Rustam Kuramshin <leo.in.empty@gmail.com>
2022-12-05 22:32:15 +0300

    commit

--- src/main/java/com/rustam/dev/JMH.java
+++ src/main/java/com/rustam/dev/JMH.java
@@ -1,20 +1,50 @@
 package com.rustam.dev;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Warmup(iterations = 1, time = 5)
+@Measurement(iterations = 1, time = 5)
+@Fork(1)
 public class JMH {
 
-    @Benchmark
-    @Fork(value = 1, warmups = 2)
-    @BenchmarkMode(Mode.AverageTime)
-    public int measureName() {
-        return sum(10000, 111111);
+    @Param({"1", "10", "100", "1000", "10000"})
+    public int howMany;
+
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(JMH.class.getSimpleName())
+                .build();
+
+        new Runner(opt).run();
     }
 
-    public static int sum(int a, int b) {
-        return a + b;
+    @Benchmark
+    public List<String> testArray() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < howMany; ++i) {
+            list.add("" + i);
+        }
+        return list;
     }
+
+    @Benchmark
+    public List<String> testLinked() {
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < howMany; ++i) {
+            list.add("" + i);
+        }
+        return list;
+    }
+
 }
