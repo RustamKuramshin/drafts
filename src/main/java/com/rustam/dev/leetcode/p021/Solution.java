ac6ee5ee4a42d5ce3b4bc68b2d8a8623c2f0481a
RustamKuramshin <kuramshin.py@yandex.ru>
2023-01-22 17:18:52 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/p021/Solution.java
+++ src/main/java/com/rustam/dev/leetcode/p021/Solution.java
@@ -1,9 +1,5 @@
 package com.rustam.dev.leetcode.p021;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-
 public class Solution {
 
     private static class ListNode {
@@ -26,76 +22,69 @@ public class Solution {
 
     private static void printListNode(ListNode listNode) {
 
-        ListNodeIterator listNodeIterator = new ListNodeIterator(listNode);
-
-        List<Integer> res = new ArrayList<>();
-
-        while (listNodeIterator.hasNext()) {
-            res.add(listNodeIterator.next());
+        if (listNode == null) {
+            System.out.println("[]");
+            return;
         }
+
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+
+        ListNode node = listNode;
+        do {
+            res.append(node.val);
+            node = node.next;
+            if (node != null) res.append(", ");
+        } while (node != null);
+
+        res.append("]");
 
         System.out.println(res);
     }
 
-    private static class ListNodeIterator {
-
-        private ListNode lastReturned;
-        private ListNode next;
-
-        public ListNodeIterator(ListNode listNode) {
-            this.next = listNode;
-        }
-
-        public boolean hasNext() {
-            return next != null;
-        }
-
-        public int next() {
-
-            if (!hasNext())
-                throw new NoSuchElementException();
-
-            lastReturned = next;
-            next = next.next;
-            return lastReturned.val;
-        }
-    }
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        ListNode result = null;
-        ListNode tail = null;
+        if (list1 == null && list2 == null) return null;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
-        ListNodeIterator listNodeIterator1 = new ListNodeIterator(list1);
-        ListNodeIterator listNodeIterator2 = new ListNodeIterator(list2);
+        ListNode node1 = list1;
+        ListNode node2 = list2;
 
-        while (true) {
+        do {
 
-            if (!(listNodeIterator1.hasNext() || listNodeIterator2.hasNext())) break;
-
-            Integer val1 = listNodeIterator1.hasNext() ? listNodeIterator1.next() : null;
-            Integer val2 = listNodeIterator2.hasNext() ? listNodeIterator2.next(): null;
-
-            ListNode n1 = new ListNode(val1);
-            ListNode n2 = new ListNode(val2);
-
-            if (val1 <= val2) {
-                n1.next = n2;
-            } else {
-                n2.next = n1;
+            if (node1 == null) {
+                break;
             }
 
-            if (result == null) {
-                result = val1 <= val2 ? n1 : n2;
-                tail = result.next;
-            } else {
-                tail.next = val1 <= val2 ? n1 : n2;
-                tail = tail.next;
+            if (node1.val == node2.val) {
+                ListNode temp = node2.next;
+                node2.next = new ListNode(node1.val);
+                node2.next.next = temp;
+                node2 = node2.next;
             }
 
-        }
+            if (node1.val < node2.val) {
+                int tempVal = node2.val;
+                ListNode temp = node2.next;
+                node2.val = node1.val;
+                node2.next = new ListNode(tempVal);
+                node2.next.next = temp;
+            }
 
-        return result;
+            if (node1.val > node2.val) {
+                ListNode temp = node2.next;
+                node2.next = new ListNode(node1.val);
+                node2.next.next = temp;
+            }
+
+            node1 = node1.next;
+            node2 = node2.next;
+
+        } while (node1 != null || node2 != null);
+
+
+        return list2;
     }
 
     public static void main(String[] args) {
@@ -112,19 +101,48 @@ public class Solution {
 
         printListNode(s.mergeTwoLists(ln1, ln2)); // [1, 1, 2, 3, 4, 4]
 
-        ln1 = null;
-        ln2 = null;
+        printListNode(s.mergeTwoLists(null, null)); // null
 
-        ln1 = new ListNode(1);
-        ln1.next = new ListNode(1);
-        ln1.next.next = new  ListNode(1);
+        ListNode ln6 = new ListNode(0);
 
-        ln2 = new ListNode(2);
-        ln2.next = new ListNode(2);
-        ln2.next.next = new ListNode(2);
+        printListNode(s.mergeTwoLists(null, ln6)); // [0]
 
-        printListNode(s.mergeTwoLists(ln1, ln2)); // [1, 1, 1, 2, 2, 2]
 
+        ListNode ln7 = new ListNode(1);
+        ln7.next = new ListNode(1);
+        ln7.next.next = new  ListNode(1);
+
+        ListNode ln8 = new ListNode(2);
+        ln8.next = new ListNode(2);
+        ln8.next.next = new ListNode(2);
+
+        printListNode(s.mergeTwoLists(ln7, ln8)); // [1, 1, 1, 2, 2, 2]
+
+        ListNode ln9 = new ListNode(1);
+
+        ListNode ln10 = new ListNode(2);
+
+        printListNode(s.mergeTwoLists(ln9, ln10)); // [1, 2]
+
+        ListNode ln11 = new ListNode(1);
+
+        ListNode ln12 = new ListNode(1);
+
+        printListNode(s.mergeTwoLists(ln11, ln12)); // [1, 1]
+
+        ListNode ln13 = new ListNode(1);
+
+        ListNode ln14 = new ListNode(0);
+
+        printListNode(s.mergeTwoLists(ln13, ln14)); // [0, 1]
+
+        ListNode ln15 = new ListNode(5);
+
+        ListNode ln16 = new ListNode(1);
+        ln16.next = new ListNode(2);
+        ln16.next.next = new  ListNode(4);
+
+        printListNode(s.mergeTwoLists(ln15, ln16)); // [1, 2, 4, 5]
     }
 }
 
