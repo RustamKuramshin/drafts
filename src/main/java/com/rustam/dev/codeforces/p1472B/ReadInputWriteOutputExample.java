d7c1934af60c9be7da6f50ebec164204cf895f42
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-05 22:50:02 +0300

    commit

---
+++ src/main/java/com/rustam/dev/codeforces/p1472B/ReadInputWriteOutputExample.java
@@ -0,0 +1,59 @@
+package com.rustam.dev.codeforces.p1472B;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ReadInputWriteOutputExample {
+
+    public static PrintWriter out;
+
+    // https://codeforces.com/blog/entry/7018?locale=ru
+    public static class Reader {
+
+        // Класс Reader может быть расширен любыми статическими методами парсинга строки
+
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        static String next() throws IOException {
+            while ( ! tokenizer.hasMoreTokens() ) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Problem1472B.Reader.init(System.in);
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Далее идет ваш код
+
+        // Пример чтения строк, целых и вещественных чисел
+        Reader.next();
+        Reader.nextInt();
+        Reader.nextDouble();
+
+
+        // Отправить накопленный вывод на stdout
+        out.close();
+    }
+}
