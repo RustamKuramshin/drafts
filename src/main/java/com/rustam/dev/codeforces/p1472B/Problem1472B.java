b80897fd3a7e654fb9a1bce1ffd8ed91d803002b
RustamKuramshin <kuramshin.py@yandex.ru>
2023-02-05 14:18:52 +0300

    commit

---
+++ src/main/java/com/rustam/dev/codeforces/p1472B/Problem1472B.java
@@ -0,0 +1,81 @@
+package com.rustam.dev.codeforces.p1472B;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem1472B {
+
+    public static PrintWriter out;
+
+    public static class Reader {
+
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /** call this method to initialize reader for InputStream */
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /** get next word */
+        static String next() throws IOException {
+            while ( ! tokenizer.hasMoreTokens() ) {
+                //TODO add check for eof if necessary
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader.init(System.in);
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int datasetsCount = Reader.nextInt();
+
+        for (int i = 0; i < datasetsCount; i++) {
+
+            int candiesCount = Reader.nextInt();
+
+            int c1 = 0, c2 = 0;
+
+            for(int j = 0; j < candiesCount; j++) {
+
+                int candy = Reader.nextInt();
+
+                if (candy == 1) {
+                    ++c1;
+                } else if (candy == 2) {
+                    ++c2;
+                }
+            }
+
+            if (c1 == 0) {
+                if (c2%2 == 0) {
+                    out.println("YES");
+                    continue;
+                }
+                out.println("NO");
+                continue;
+            }
+
+            if (c1%2 == 0) {
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
+        }
+
+        out.close();
+    }
+}
+
