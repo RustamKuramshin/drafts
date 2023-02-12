60b3e7215735b6c1364596d8acc0664d7b113132
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-12 15:36:09 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/p146/LRUCache.java
+++ src/main/java/com/rustam/dev/leetcode/p146/LRUCache.java
@@ -5,6 +5,35 @@ public class LRUCache {
     private Node head;
     private Node last;
 
+    private int size;
+
+    private static void printFromNode(Node fromNode) {
+
+        if (fromNode == null) {
+            System.out.println("[]");
+            return;
+        }
+
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+
+        Node node = fromNode;
+        do {
+            res.append(node.val);
+            node = node.next;
+            if (node != null) res.append(", ");
+        } while (node != null);
+
+        res.append("]");
+
+        System.out.println(res);
+    }
+
+    public void printCache() {
+        System.out.print("cache: ");
+        printFromNode(this.head);
+    }
+
     private static class Node {
 
         int val;
@@ -24,14 +53,54 @@ public class LRUCache {
 
     private int capacity;
 
+    private void appendNodeToEnd(Node node) {
+        node.prev = this.last;
+        this.last.next = node;
+        this.last = node;
+    }
+
+    private void removeNode(Node node) {
+
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+
+        // удаление из середины
+        if (prevNode != null && nextNode != null) {
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+        }
+
+        // удаление с головы
+        if (prevNode == null) {
+            this.head = nextNode;
+            nextNode.prev = null;
+        }
+
+        // удаление с хвоста
+        if (nextNode == null) {
+            this.last = prevNode;
+            prevNode.next = null;
+        }
+
+        node.prev = null;
+        node.next = null;
+    }
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        this.size = 0;
         this.table = new Node[10000];
     }
 
     public int get(int key) {
-        Node res = this.table[key];
-        return res == null ? -1 : res.val;
+        Node node = this.table[key];
+
+        if (node != null) {
+            removeNode(node);
+            appendNodeToEnd(node);
+        }
+
+        return node == null ? -1 : node.val;
     }
 
     public void put(int key, int value) {
@@ -42,9 +111,7 @@ public class LRUCache {
             this.head = newNode;
             this.last = this.head;
         } else {
-            newNode.prev = this.last;
-            this.last.next = newNode;
-            this.last = newNode;
+            appendNodeToEnd(newNode);
         }
 
         this.table[key] = newNode;
@@ -55,12 +122,22 @@ public class LRUCache {
         LRUCache lRUCache = new LRUCache(2);
         lRUCache.put(1, 1); // cache is {1=1}
         lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        System.out.println("get()");
         System.out.println(lRUCache.get(1));    // return 1
+        lRUCache.printCache();
         lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        System.out.println("get()");
         System.out.println(lRUCache.get(2));    // returns -1 (not found)
+        lRUCache.printCache();
         lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        System.out.println("get()");
         System.out.println(lRUCache.get(1));    // return -1 (not found)
+        lRUCache.printCache();
+        System.out.println("get()");
         System.out.println(lRUCache.get(3));    // return 3
+        lRUCache.printCache();
+        System.out.println("get()");
         System.out.println(lRUCache.get(4));    // return 4
+        lRUCache.printCache();
     }
 }
