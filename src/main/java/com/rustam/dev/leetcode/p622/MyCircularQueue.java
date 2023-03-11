81104691c6ad7426fe2c0fb94768f45565fa9d42
RustamKuramshin <kuramshin.py@yandex.ru>
2023-03-11 19:01:01 +0300

    commit

---
+++ src/main/java/com/rustam/dev/leetcode/p622/MyCircularQueue.java
@@ -0,0 +1,43 @@
+package com.rustam.dev.leetcode.p622;
+
+public class MyCircularQueue {
+
+    // Инициализирует объект размером очереди k
+    public MyCircularQueue(int k) {
+
+    }
+
+    // Вставляет элемент в циклическую очередь. Возвращает true, если операция выполнена успешно
+    public boolean enQueue(int value) {
+        return false;
+    }
+
+    // Удаляет элемент из циклической очереди. Возвращает true, если операция выполнена успешно
+    public boolean deQueue() {
+        return false;
+    }
+
+    // Получает первый элемент из очереди. Если очередь пуста, вернуть -1
+    public int Front() {
+        return 0;
+    }
+
+    // Получает последний элемент из очереди. Если очередь пуста, вернуть -1
+    public int Rear() {
+        return 0;
+    }
+
+    // Проверяет, пуста ли круговая очередь
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // Проверяет, заполнена ли круговая очередь
+    public boolean isFull() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
