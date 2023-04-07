0ecb580e3a54ea76d012cc74f11506a3b710731c
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-04-07 16:32:06 +0300

    commit

--- src/main/java/com/rustam/dev/ArrayShuffle.java
+++ src/main/java/com/rustam/dev/ArrayShuffle.java
@@ -24,9 +24,9 @@ public class ArrayShuffle {
         Random rand = new Random();
 
         for (int i = 0; i < array.length; i++) {
-            int randomIndexToSwap = rand.nextInt(array.length);
-            int temp = array[randomIndexToSwap];
-            array[randomIndexToSwap] = array[i];
+            int swapIdx = rand.nextInt(array.length);
+            int temp = array[swapIdx];
+            array[swapIdx] = array[i];
             array[i] = temp;
         }
         return array;
