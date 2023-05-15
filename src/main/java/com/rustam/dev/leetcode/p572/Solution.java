95c1a479fbb5960d63eeeb5dc30a83634d93893e
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-05-15 17:47:28 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/p572/Solution.java
+++ src/main/java/com/rustam/dev/leetcode/p572/Solution.java
@@ -5,6 +5,8 @@ import static com.rustam.dev.leetcode.LeetCodeUtils.TreeNode;
 // https://leetcode.com/problems/subtree-of-another-tree/
 public class Solution {
 
+    private boolean res = false;
+
     public boolean equals(TreeNode treeNode1, TreeNode treeNode2) {
         if (treeNode1 == treeNode2) return true;
         return treeNode1 != null && treeNode2 != null && treeNode1.val == treeNode2.val &&
@@ -12,38 +14,16 @@ public class Solution {
                 equals(treeNode1.right, treeNode2.right);
     }
 
-    private boolean traversal(TreeNode root, TreeNode subRoot, boolean res) {
-        if (root == null) {
-            System.out.println("recursion base");
-            return res;
-        }
-
-        System.out.println("tree node value: " + root.val);
-
+    private void traversal(TreeNode root, TreeNode subRoot) {
+        if (root == null) return;
+        if (res) return;
         res = equals(root, subRoot);
-
-        if (res) {
-            System.out.println("return - 1");
-            return true;
-        }
-
-        res = traversal(root.left, subRoot, res);
-        if (res) {
-            System.out.println("return - 2");
-            return true;
-        }
-
-        res = traversal(root.right, subRoot, res);
-        if (res) {
-            System.out.println("return - 3");
-            return true;
-        };
-
-        System.out.println("return - 4");
-        return res;
+        traversal(root.left, subRoot);
+        traversal(root.right, subRoot);
     }
 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        return traversal(root, subRoot, false);
+        traversal(root, subRoot);
+        return res;
     }
 }
