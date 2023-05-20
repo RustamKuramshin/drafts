8b8a2fad39dc10ea917bccd532ab3855556d7a9b
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-05-21 02:59:52 +0300

    commit

--- sync-push-leetcode-utils-changes.sh
+++ sync-push-leetcode-utils-changes.sh
@@ -8,5 +8,5 @@ NEW_PACKAGE_NAME="package leetcode.utils"
 pwd && ls -la && \
 cat ./src/main/java/com/rustam/dev/leetcode/LeetCodeUtils.java > ../leetcode-utils/src/main/java/leetcode/utils/LeetCodeUtils.java && \
 cd .. && ls -la && cd leetcode-utils && pwd && ls -la && cd ./src/main/java/leetcode/utils && ls -ls && \
-gsed -i "s/${COPIED_PACKAGE_NAME}/${NEW_PACKAGE_NAME}/g" LeetCodeUtils.java
+gsed -i "s/${COPIED_PACKAGE_NAME}/${NEW_PACKAGE_NAME}/g" LeetCodeUtils.java && \
 git status && git add -A && git commit -m "init commit" && git push -u origin main
