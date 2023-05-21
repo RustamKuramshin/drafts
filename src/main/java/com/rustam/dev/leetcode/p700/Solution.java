8e76daea11f69c47d7ddc0e8c15609201661caf1
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-05-21 13:06:51 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/p700/Solution.java
+++ src/main/java/com/rustam/dev/leetcode/p700/Solution.java
@@ -1,19 +1,41 @@
 package com.rustam.dev.leetcode.p700;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static com.rustam.dev.leetcode.LeetCodeUtils.TreeNode;
 
 // https://leetcode.com/problems/search-in-a-binary-search-tree/
 public class Solution {
 
-    public TreeNode searchBST(TreeNode root, int val) {
-        System.gc();
-        if (root != null && val == root.val) {
-            return root;
-        } else if (root != null && val > root.val) {
-            return searchBST(root.right, val);
-        } else if (root != null && val < root.val) {
-            return searchBST(root.left, val);
+    private TreeNode traverse(TreeNode root, int val) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root != null) queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            TreeNode node = queue.poll();
+
+            if (node.val == val) {
+                System.gc();
+                return node;
+            }
+
+            if (node.left != null && val < node.val) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null && val > node.val) {
+                queue.offer(node.right);
+            }
         }
+
         return null;
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        return traverse(root, val);
     }
 }
