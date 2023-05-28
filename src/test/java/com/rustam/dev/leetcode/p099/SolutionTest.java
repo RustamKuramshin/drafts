2d3b1740d3a6b88779032652faa2edba0b75fa9e
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-05-28 16:49:26 +0300

    commit

---
+++ src/test/java/com/rustam/dev/leetcode/p099/SolutionTest.java
@@ -0,0 +1,41 @@
+package com.rustam.dev.leetcode.p099;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.rustam.dev.leetcode.LeetCodeUtils.TreeNode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Тестирование метода восстановление структуры двоичного дерева поиска")
+public class SolutionTest {
+
+    @Test
+    @DisplayName("Базовый пример Example 1")
+    public void test_example_1() {
+        //arrange
+        TreeNode root = TreeNode.ofArrayString("[1,3,null,null,2]");
+        TreeNode expected = TreeNode.ofArrayString("[3,1,null,null,2]");
+
+        //act
+        Solution s = new Solution();
+        s.recoverTree(root);
+
+        //assert
+        assertEquals(expected, root);
+    }
+
+    @Test
+    @DisplayName("Базовый пример Example 2")
+    public void test_example_2() {
+        //arrange
+        TreeNode root = TreeNode.ofArrayString("[3,1,4,null,null,2]");
+        TreeNode expected = TreeNode.ofArrayString("[2,1,4,null,null,3]");
+
+        //act
+        Solution s = new Solution();
+        s.recoverTree(root);
+
+        //assert
+        assertEquals(expected, root);
+    }
+}
