f5a7e76d5bd3f7fa3ef4887a799a146927032f46
RustamKuramshin <kuramshin.py@yandex.ru>
2023-08-13 01:09:32 +0300

    commit

--- src/main/java/com/rustam/dev/Test.java
+++ src/main/java/com/rustam/dev/Test.java
@@ -1,25 +1,7 @@
 package com.rustam.dev;
 
-import java.util.TreeMap;
-
 class Test {
     public static void main(String[] args) {
-
-        TreeMap<Integer, String> tm = new TreeMap<>();
-
-        tm.put(10, "aer");
-        tm.put(123, "aer");
-        tm.put(1, "aer");
-        tm.put(4564, "aer");
-        tm.put(3456, "aer");
-        tm.put(4, "aer");
-
-        System.out.println(tm);
-
-        tm.remove(1);
-
-        System.out.println(tm);
-
     }
 }
 
