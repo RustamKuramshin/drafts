9bdf0b99c9223eb0e01d136f33fdb7b4c184cece
Rustam Kuramshin <kuramshin.py@yandex.ru>
2023-09-04 17:43:35 +0300

    commit

---
+++ src/main/java/com/rustam/dev/BracketsValidator.java
@@ -0,0 +1,24 @@
+package com.rustam.dev;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class BracketsValidator {
+    
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
