fcef17e9133d18caf28d9604d9c0bf4697f902e7
Rustam Kuramshin <kuramshin.py@yandex.ru>
2023-09-29 13:34:31 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/p169/Solution.java
+++ src/main/java/com/rustam/dev/leetcode/p169/Solution.java
@@ -26,6 +26,20 @@ public class Solution {
         return -1;
     }
 
+    public int majorityElement2(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
 
