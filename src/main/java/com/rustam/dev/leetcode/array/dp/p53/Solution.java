aca9b01df18b43385849a9fd91df56f3178ab180
Rustam Kuramshin <kuramshin.py@yandex.ru>
2024-03-03 15:47:29 +0300

    commit

--- src/main/java/com/rustam/dev/leetcode/array/dp/p53/Solution.java
+++ src/main/java/com/rustam/dev/leetcode/array/dp/p53/Solution.java
@@ -25,7 +25,37 @@ public class Solution {
         return maxSum;
     }
 
+    // recursive
+    private int maxSum = Integer.MIN_VALUE;
+
+    private void recursiveSum(int[] nums, int i, int sum, boolean inSubArr) {
+        if (i >= nums.length) return;
+
+        if (inSubArr) {
+            sum += nums[i];
+            if (sum > maxSum) maxSum = sum;
+            recursiveSum(nums, i + 1, sum, true);
+        } else {
+            recursiveSum(nums, i, 0, true);
+            recursiveSum(nums, i + 1, 0, false);
+        }
+    }
+
     public int maxSubArray2(int[] nums) {
-        return 0;
+        if (nums.length == 1) return nums[0];
+        recursiveSum(nums, 0, 0, false);
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+//        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {5, 4, -1, 7, 8};
+
+        int maxSum = s.maxSubArray2(arr);
+
+        System.out.println(maxSum);
     }
 }
