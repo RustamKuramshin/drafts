fa4d8b7ff026fc2f46a8f0c0a8e7ac8accce4084
RustamKuramshin <leo.in.empty@gmail.com>
2025-01-29 23:35:10 +0300

    commit

--- src/main/kotlin/com/rustam/dev/Test.kt
+++ src/main/kotlin/com/rustam/dev/Test.kt
@@ -1,5 +1,15 @@
 package com.rustam.dev
 
-fun main() {
+import java.util.stream.Stream
 
+fun main() {
+    val list = listOf("qw", "qwe", "qwer", "qwerty")
+
+    val even = list.stream().filterEvenLength().toList()
+
+    println(even)
+}
+
+fun Stream<String>.filterEvenLength(): Stream<String> {
+    return this.filter { it.length%2 == 0 }
 }
